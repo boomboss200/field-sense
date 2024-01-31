d124f3c207b030cf044cb624d93c89f3f8c237de
Behzad Khadim <98022071+boomboss200@users.noreply.github.com>
2024-02-01 02:56:59 +0500

    updated vercel

--- djangoauthapi1/build_files.sh
+++ djangoauthapi1/build_files.sh
@@ -1,4 +1,6 @@
 echo "BUILD START"
+    python3.10  pip install --upgrade pip
+    python3.10  pip install --upgrade pip setuptools
  python3.10  -m pip install -r requirements.txt
  python3.10  manage.py collectstatic --noinput --clear
  echo "BUILD END"
