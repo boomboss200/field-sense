db4e1eac3f53512547877da8ecc60908036cbe81
Behzad Khadim <98022071+boomboss200@users.noreply.github.com>
2024-02-01 02:50:08 +0500

    python version update

--- djangoauthapi1/build_files.sh
+++ djangoauthapi1/build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
-    python3.9  pip install --upgrade pip
-    python3.9  pip install --upgrade pip setuptools
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
+    python3.10.9  pip install --upgrade pip
+    python3.10.9  pip install --upgrade pip setuptools
+ python3.10.9  -m pip install -r requirements.txt
+ python3.10.9  manage.py collectstatic --noinput --clear
  echo "BUILD END"
