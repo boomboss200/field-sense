ee9e8d1d8fc549d7ac0d4f4a9ac0035591578ec7
Behzad Khadim <98022071+boomboss200@users.noreply.github.com>
2024-02-01 03:34:05 +0500

    yy

--- djangoauthapi1/build_files.sh
+++ djangoauthapi1/build_files.sh
@@ -1,6 +1,7 @@
 echo "BUILD START"
     python3 pip install --upgrade pip
     python3 pip install --upgrade pip setuptools
+    python3 pip install --upgrade pip Django
  python3 -m pip install -r requirements.txt
  python3 manage.py collectstatic --noinput --clear
  echo "BUILD END"
