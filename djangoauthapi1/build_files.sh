e16b6510a708211712e25205dfca453ae7662d9f
Behzad Khadim <98022071+boomboss200@users.noreply.github.com>
2024-02-01 03:06:07 +0500

    o

--- djangoauthapi1/build_files.sh
+++ djangoauthapi1/build_files.sh
@@ -1,7 +1,7 @@
 echo "BUILD START"
-    python3 pip3 install --upgrade pip
+    python3 pip install --upgrade pip
     python3 pip install --upgrade pip setuptools
-    python3 pip3 install tensorflow==2.15.0
+    python3 pip install django
 
  python3 -m pip install -r requirements.txt
  python3 manage.py collectstatic --noinput --clear
