c5747ec8739042f7af6ba759567818552f9a55d6
Behzad Khadim <98022071+boomboss200@users.noreply.github.com>
2024-02-26 13:51:55 +0500

    pip urllib

--- djangoauthapi1/build_files.sh
+++ djangoauthapi1/build_files.sh
@@ -3,6 +3,7 @@ echo "BUILD START"
     python3 pip install --upgrade pip setuptools
     python3 -m pip install django==4.0.3
     python3 -m pip install earthengine-api
+    python3 -m pip install urllib3==1.26.7
     # python3 -m pip install urllib3==1.0.2
  python3 -m pip install -r requirements.txt
  python3 manage.py collectstatic --noinput --clear
