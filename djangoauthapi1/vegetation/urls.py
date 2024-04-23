e899d9326b83efecb1c829e8a067eb8af6ce11be
Behzad Khadim <bkicloud2001@gmail.com>
2024-04-23 16:08:26 +0500

    final updates

--- djangoauthapi1/vegetation/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'vegetation'
-
-urlpatterns = [
-    path('pointwise/', views.point_wise_ndvi),
-    path('average/', views.average_ndvi),    
-]
